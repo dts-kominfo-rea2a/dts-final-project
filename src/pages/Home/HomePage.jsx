0a7dad7ee5aa79b5209749c3466d40b7942931f0
Bardizba <bardizba@mifx.com>
2022-08-04 19:41:55 +0700

    feat(homepage): guest only

--- src/pages/Home/HomePage.jsx
+++ src/pages/Home/HomePage.jsx
@@ -1,42 +1,26 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import Logo from "../../assets/logo.svg"
 import DefaultButton from "../../components/Buttons/DefaultButton";
 import SecondaryButton from "../../components/Buttons/SecondaryButton";
-import { Link, useNavigate } from "react-router-dom";
-import { useSelector } from "react-redux";
-import { selectIsLoggedIn } from "../../store/userStore";
+import { Link} from "react-router-dom";
 import useTitle from "../../hooks/useTitle";
 
 const HomePage = () => {
-    const isLoggedin = useSelector(selectIsLoggedIn)
-    const navigate = useNavigate()
-    const [loading, setLoading] = useState(true)
-
-    useEffect(() => {
-        setLoading(false)
-        if (isLoggedin) {
-            navigate('/timeline')
-        }
-    }, [isLoggedin, navigate])
-
     useTitle('Welcome to Miaowbook')
     return (
         <div>
-            {
-                !loading && (
-                    <>
-                        <img src={Logo} className="w-16 shadow-2xl mb-40" alt="MiaowBook Logo" />
-                        <div className="text-5xl mb-10">Share your animal's story with the world!</div>
-                        <h1 className="text-3xl">Join <span className="font-bold">MiaowBook</span> today.</h1>
-                        <div className="mt-8 mb-5 gap-2 flex">
-                            <Link to="/auth/register">
-                                <SecondaryButton>Create an Account</SecondaryButton>
-                            </Link>
-                            <Link to="/auth/login"><DefaultButton>Login</DefaultButton></Link>
-                        </div>
-                    </>
-                )
-            }
+
+
+            <img src={Logo} className="w-16 shadow-2xl mb-40" alt="MiaowBook Logo" />
+            <div className="text-5xl mb-10">Share your animal's story with the world!</div>
+            <h1 className="text-3xl">Join <span className="font-bold">MiaowBook</span> today.</h1>
+            <div className="mt-8 mb-5 gap-2 flex">
+                <Link to="/auth/register">
+                    <SecondaryButton>Create an Account</SecondaryButton>
+                </Link>
+                <Link to="/auth/login"><DefaultButton>Login</DefaultButton></Link>
+            </div>
+
         </div>
     )
 }
--- src/pages/Templates/HomepageTemplate.jsx
+++ src/pages/Templates/HomepageTemplate.jsx
@@ -1,32 +1,50 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Container from "../../components/Container"
 import BgPhoto from "../../assets/images/bg.jpg"
 import Logo from "../../assets/logo.svg"
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
+import { selectIsLoggedIn } from "../../store/userStore";
+import { useSelector } from "react-redux";
 
 const HomepageTemplate = () => {
+    const isLoggedin = useSelector(selectIsLoggedIn)
+    const navigate = useNavigate()
+    const [loading, setLoading] = useState(true)
+
+    useEffect(() => {
+        setLoading(false)
+        if (isLoggedin) {
+            navigate('/timeline')
+        }
+    }, [isLoggedin, navigate])
     return (
-        <Container size="2xl">
-            <div className="flex flex-col-reverse md:flex-row w-100 min-h-screen">
-                <div
-                    className="w-full md:w-1/2"
-                    style={
-                        { background: `url(${BgPhoto}) center / cover no-repeat` }
-                    }>
-                    <div className="bg-gray-800 w-full h-full bg-opacity-70 text-white">
-                        <div className="w-full h-full flex justify-center items-center">
-                            <div className="flex flex-col text-center gap-2 opacity-75 py-8 h-96 md:h-full">
-                                {/* <img src={Logo} className="w-32 md:w-64 shadow-2xl" alt="MiaowBook Logo" /> */}
-                                {/* <span>Social Media for Pets</span> */}
+        <>
+            {
+                !loading && (
+                    <Container size="2xl">
+                        <div className="flex flex-col-reverse md:flex-row w-100 min-h-screen">
+                            <div
+                                className="w-full md:w-1/2"
+                                style={
+                                    { background: `url(${BgPhoto}) center / cover no-repeat` }
+                                }>
+                                <div className="bg-gray-800 w-full h-full bg-opacity-70 text-white">
+                                    <div className="w-full h-full flex justify-center items-center">
+                                        <div className="flex flex-col text-center gap-2 opacity-75 py-8 h-96 md:h-full">
+                                            {/* <img src={Logo} className="w-32 md:w-64 shadow-2xl" alt="MiaowBook Logo" /> */}
+                                            {/* <span>Social Media for Pets</span> */}
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="w-full md:w-1/2 p-5">
+                                <Outlet />
                             </div>
                         </div>
-                    </div>
-                </div>
-                <div className="w-full md:w-1/2 p-5">
-                    <Outlet />
-                </div>
-            </div>
-        </Container>
+                    </Container>
+                )
+            }
+        </>
     )
 }
 
